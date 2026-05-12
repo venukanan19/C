fc15913634b06c9d810e5c0d3be5813da42b47a3
Venukanan19 <venumano2002@gmail.com>
2026-05-12 21:14:18 +0530

    updated files

--- Day1_Activity1_Console new/obj/Debug/net8.0/Day1_Activity1_Console new.AssemblyInfo.cs
+++ Day1_Activity1_Console new/obj/Debug/net8.0/Day1_Activity1_Console new.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1_Activity1_Console new")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a8d99a441355f81dc7741885a6c9ad8fbe7aa1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1_Activity1_Console new")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1_Activity1_Console new")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
